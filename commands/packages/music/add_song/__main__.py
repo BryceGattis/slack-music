638b53f6f9c9b317950ff8ac7d327d9e5b238e37
Bryce Gattis <brycegattis@yahoo.com>
2023-08-06 13:01:04 -0500

    Push latest mostly functional add song functionality. Still need to figure out oauth for the button press.

--- commands/packages/music/add_song/__main__.py
+++ commands/packages/music/add_song/__main__.py
@@ -6,12 +6,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 LOG = logging.getLogger(__name__)
 
 
-def main(args):
+def main(event):
     logging.basicConfig(level=logging.INFO)
-    LOG.info(args)
-    LOG.info(type(args))
     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
-    resp = sp.search("A song name", type="track", limit=1)
+    resp = sp.search(event['text'], type="track", limit=3)
     if resp.get('error'):
         return {
             "body": {
@@ -19,38 +17,53 @@ def main(args):
                 "text": resp['error']['message']
             }
         }
-    song_names = []
+    track_infos = []
     for track in resp['tracks']['items']:
-        song_names.append(track['name'])
-    blocks_data = []
-    for song_name in song_names:
-        block_data = {
+        track_info = dict()
+        track_info['track_id'] = track['id']
+        track_info['track_name'] = track['name']
+        track_info['album_name'] = track['album']['name']
+        track_info['thumbnail'] = track['album']['images'][0]
+        track_info['artist'] = track['artists'][0]['name']
+        track_infos.append(track_info)
+    blocks = []
+    for track_info in track_infos:
+        text = f"*{track_info['track_name']}*\n"
+        text += f"*Artist:* {track_info['artist']}\n"
+        text += f"*Album:* {track_info['album_name']}"
+        track_section = {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": song_name
+                "text": text
+            },
+            "accessory": {
+                "type": "image",
+                "image_url": track_info['thumbnail']['url'],
+                "alt_text": "Album Thumbnail"
             }
         }
-        blocks_data.append(block_data)
+        blocks.append(track_section)
+        add_button_section = {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Add to playlist"
+                    },
+                    "style": "primary",
+                    "value": "moo",
+                    "action_id": "add_song_to_playlist"
+                },
+            ]
+        }
+        blocks.append(add_button_section)
     return {
         "body": {
             "text": "Hello World",
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "*It's 80 degrees right now.*"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Partly cloudy today and tomorrow"
-                    }
-                }
-            ],
+            "blocks": blocks,
             "response_type": "ephemeral",
         },
         "statusCode": 200,
